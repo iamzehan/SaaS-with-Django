2b0ea9386c74d2d0cb0467282d8b1113c828af4c
Md. Ziaul Karim <zehan.kareem@gmail.com>
2025-05-28 21:03:42 +0600

    Add dashboard view

--- saasboard/src/dashboard/views.py
+++ saasboard/src/dashboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def dashboard_view(request):
+    return render(request, "dashboard/main.html", {})
