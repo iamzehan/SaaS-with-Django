730ecb1955905c018d0e84ef39ae72701cee5bb6
Md. Ziaul Karim <zehan.kareem@gmail.com>
2025-05-28 20:25:18 +0600

    Some rearrangement

--- saasboard/src/landing/views.py
+++ saasboard/src/landing/views.py
@@ -13,4 +13,9 @@ def landing_dashboard_page_view(request):
     PageVisit.objects.create(path=request.path)
     page_views_formatted = helpers.numbers.shorten_number(qs.count() * 100_000)
     social_views_formatted = helpers.numbers.shorten_number(qs.count() * 23_000)
-    return render(request, "landing/main.html", {"page_view_count": page_views_formatted, "social_views_count": social_views_formatted})
+    return render(request, 
+                  "landing/main.html", 
+                  {
+                    "page_view_count": page_views_formatted,
+                    "social_views_count": social_views_formatted
+                })
