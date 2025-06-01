8d416fa24d4a46a6fc9687106bf5790abe32c5ed
Zehan <zehan.kareem@gmail.com>
2025-06-01 21:24:11 +0600

    Update url for list view

--- saasboard/src/profiles/urls.py
+++ saasboard/src/profiles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.profile_list_view),
+    path("lists", views.profile_list_view),
     path("<str:username>/", views.profile_detail_view),
 ]
